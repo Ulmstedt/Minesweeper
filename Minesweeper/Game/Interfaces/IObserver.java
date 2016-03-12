323027fbc0a284f17f2b4aa9d76d0793fbf9c30a
Ulmstedt <matte___93@hotmail.com>
2016-03-12 12:05:07 +0100

    Add moveMade to IObserver

--- Minesweeper/Game/Interfaces/IObserver.java
+++ Minesweeper/Game/Interfaces/IObserver.java
@@ -1,9 +1,12 @@
 package Minesweeper.Game.Interfaces;
 
+import Minesweeper.Game.Move;
+
 /**
  *
  * @author Sehnsucht
  */
 public interface IObserver {
     public void gameEnded(boolean win, int blocksRevealed, int blocksLeft);
+    public void moveMade(Move move);
 }
